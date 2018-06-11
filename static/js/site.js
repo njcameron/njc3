0e332b630b7f3d16b6b02e0b6c0d1349e49f6b72
Neil Cameron <neil@neilcameron.me>
2018-06-11 19:21:01 +0100

    werk

--- static/js/site.js
+++ static/js/site.js
@@ -1,26 +1,23 @@
 $( document ).ready(function() {
   // Set blur on image on mouse over.
-  let hover_blur = $('.work-item');
-
+  var hover_blur = $('.work-item');
   // Set blur on image on mouse over.
   hover_blur.mouseenter(function() {
-    let that = this;
-    setTimeout(function(){
-      $(that).find('img').addClass('blur');
-    },250);
-
+      $(this).find('img').addClass('blur');
+      $(this).find('.info-layer').show();
   });
 
   // On mouse leave remove blur.
   hover_blur.mouseleave(function() {
-    let workImage = $(this).find('img');
+    var workImage = $(this).find('img');
     workImage.removeClass('blur');
+    $(this).find('.info-layer').hide();
   });
 
   // On click remove blur and hover layer.
   hover_blur.click(function() {
-    let workImage = $(this).find('img');
+    var workImage = $(this).find('img');
     workImage.removeClass('blur');
-    $(this).find('.layer').hide();
+    $(this).find('.info-layer').hide();
   });
 });
